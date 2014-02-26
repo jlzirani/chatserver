e6e9b328ed2dab5776598fe09d60301bc5b677e6
Zirani Jean-Luc <jlzirani@gmail.com>
2014-02-26 15:09:45 +0100

    the logger is now a variadic function

--- src/log.c
+++ src/log.c
@@ -6,29 +6,46 @@
 const char *levels[] = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
 unsigned int triggerLevel = 0;
 
-
-void loggingVoid(unsigned int level, const char *msg, const char* file, const char* func, int line)
+void loggingVoid(const char* file, const char* func, int line, unsigned int level, const char *msg, va_list vargs)
 {
 }
 
-
-void loggingNormal(unsigned int level, const char* msg, const char* file, const char* func, int line)
+void loggingNormal(const char* file, const char* func, int line, unsigned int level, const char *msg, va_list vargs)
 {
 	if(level > 4) level = 4;
 	if(level >= triggerLevel)
-		printf("[%s:%s:%i] - %s: %s\n", file, func, line, levels[level], msg);
+	{
+		printf("[%s:%s:%i] - %s: ", file, func, line, levels[level]);
+		vprintf(msg, vargs);
+		printf("\n");
+	}
 }
 
-void loggingColor(unsigned int level, const char* msg, const char* file, const char* func, int line)
+void loggingColor(const char* file, const char* func, int line, unsigned int level, const char *msg, va_list vargs)
 {
 	if(level > 4) level = 4;
 	if(level >= triggerLevel)
-		printf("[\x1b[31m%s\x1b[0m:\x1b[32m%s\x1b[0m:\x1b[34m%i\x1b[0m] - %s : %s\n", file, func, line, levels[level], msg);
+	{
+		printf("[\x1b[31m%s\x1b[0m:\x1b[32m%s\x1b[0m:\x1b[34m%i\x1b[0m] - %s : ", file, func, line, levels[level]);
+		vprintf(msg, vargs);
+		printf("\n");
+	}
 }
+
+
+void (*logging)(const char* file, const char* func, int line, unsigned int level, const char* msg, va_list varg) = loggingVoid;
+
+void loggin( const char* file, const char *func, int line, int level, const char *msg, ...) 
+{
+	va_list arg;
+	va_start(arg, msg);
+	(*logging)(file, func, line, level, msg, arg);
+	va_end(arg);
+}
+
 
 void init_logger(const char *logger, unsigned int level)
 {
-	logging = &loggingVoid;
 	if(logger != 0)
 	{
 		if(strncmp(logger, "stdout", 6) == 0)
--- src/log.h
+++ src/log.h
@@ -1,9 +1,12 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
-void (*logging)(unsigned int level, const char* msg, const char* file, const char* func, int line);
+#include <stdarg.h>
+
 void init_logger(const char *logger, unsigned int level);
 
-#define logging(level,msg)	(*logging)(level, msg, __FILE__, __FUNCTION__, __LINE__)
+void loggin( const char* file, const char *func, int line, int level,  const char *msg, ... );
+
+#define logging( level,  ... )	loggin(__FILE__, __FUNCTION__, __LINE__, level,  __VA_ARGS__ ) 
 
 #endif
